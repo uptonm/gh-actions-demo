45d73d45feb12af060c749e841b02dc957a72659
Mike Upton <uptonm@wit.edu>
2019-11-14 14:41:51 -0800

    Adding new func

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,10 @@
 const sum = require('./sum');
+const factorial = require('./factorial');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('factorial of 3 === 6', () => {
+  expect(factorial(10)).toBe(3628800);
 });
